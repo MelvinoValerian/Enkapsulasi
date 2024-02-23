4d95d4ecbf8eff88483f4527721ee8df133a94f9
MelvinoValerian <151483191+MelvinoValerian@users.noreply.github.com>
2024-02-23 10:02:17 +0700

    Add files via upload

---
+++ enkapsulasi/src/User/Main.java
@@ -0,0 +1,18 @@
+
+package User;
+
+public class Main {
+    public static void main(String[] args) {
+        User user = new User("aaa","bbb");
+        User user1 = new User();
+        
+        user.setUsername("LOL");
+        user.setPassword("LIL");
+        user.setStatus(false);
+        user.setId(0);
+        
+        System.out.println("Username : "+user.getUsername());
+        System.out.println("Username : "+user.getPassword());
+        
+    }
+}
